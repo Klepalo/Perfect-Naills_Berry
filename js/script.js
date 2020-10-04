8dd5be28a2c92d9c0c6756916f4b7df5107e6034
Klepalo <69232937+Klepalo@users.noreply.github.com>
2020-10-04 21:03:05 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function () {
+  jQuery(".header_burger").click(function (event) {
+    jQuery(".header_burger,.header_menu").toggleClass("active");
+    jQuery("body").toggleClass("lock");
+  });
+});
